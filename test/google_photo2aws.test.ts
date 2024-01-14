27d1f31efb856fdf750bd507954d0d3f6e0cd680
moririn2528 <strangenoise1@gmail.com>
2024-01-14 16:49:29 +0900

    Initial commit

---
+++ test/google_photo2aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GooglePhoto2Aws from '../lib/google_photo2aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/google_photo2aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GooglePhoto2Aws.GooglePhoto2AwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
